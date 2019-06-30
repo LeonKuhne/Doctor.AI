bd119e3d358b4a5834c3c371a26156033ea26dec
Leon Kuhne <lxk1170@rit.edu>
2019-06-30 17:36:02 -0400

    saving

--- doctor.py
+++ doctor.py
@@ -6,10 +6,14 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.utils import to_categorical
 from keras.callbacks import TensorBoard
+from tensorflow.keras import metrics
 
 # config
+INIT_DENSITY = 1000
 FOOD_FILE = "DR1IFF_I"
+drug_question = 'RXDDRUG'
 food_questions = ["DR1IGRMS", "DR1IKCAL", "DR1IPROT", "DR1ICARB", "DR1ISUGR", "DR1IFIBE", "DR1ITFAT", "DR1ISFAT", "DR1IMFAT", "DR1IPFAT", "DR1ICHOL", "DR1IATOC", "DR1IATOA", "DR1IRET", "DR1IVARA", "DR1IACAR", "DR1IBCAR", "DR1ICRYP", "DR1ILYCO", "DR1ILZ", "DR1IVB1", "DR1IVB2", "DR1INIAC", "DR1IVB6", "DR1IFOLA", "DR1IFA", "DR1IFF", "DR1IFDFE", "DR1ICHL", "DR1IVB12", "DR1IB12A", "DR1IVC", "DR1IVD", "DR1IVK", "DR1ICALC", "DR1IPHOS", "DR1IMAGN", "DR1IIRON", "DR1IZINC", "DR1ICOPP", "DR1ISODI", "DR1IPOTA", "DR1ISELE", "DR1ICAFF", "DR1ITHEO", "DR1IALCO", "DR1IMOIS", "DR1IS040", "DR1IS060", "DR1IS080", "DR1IS100", "DR1IS120", "DR1IS140", "DR1IS160", "DR1IS180", "DR1IM161", "DR1IM181", "DR1IM201", "DR1IM221", "DR1IP182", "DR1IP183", "DR1IP184", "DR1IP204", "DR1IP205", "DR1IP225", "DR1IP226"]
+MIN_THRESHOLD = 0.00001
 
 # setup tensorboard
 tensorboard = TensorBoard(log_dir=f"logs/doctor-{int(time.time())}")
@@ -20,25 +24,36 @@ food_data = pd.read_sas(f"data/{FOOD_FILE}.xpt")
 
 # combine into one table
 data = pd.merge(medical_data, food_data, left_on="SEQN", right_on="SEQN")
-#data.sort_values('RXDDRUG')
+select_columns = np.concatenate(([drug_question], food_questions), axis=0)
+data = data[select_columns]
+data = data.dropna() # remove nan values
+#data.sort_values('RXDDRUG') # sort data
+
+print(data)
 
 # extract columns
 # RXDDRUG - perscription medications
 #food_data = data[FOOD_QUESTION]
-drug_data = data['RXDDRUG']
+drug_data = data[drug_question]
 #drug_ids = data.groupby('RXDDRUG').ngroup()
 drug_categories = drug_data.values
 
 def get_food_question_arr():
-    # add all questions together
-    food_question_maxs = []
+    maxs = []
     x = data[food_questions].values
 
-    # normalize between 0 and 1
     for col_id in range(len(food_questions[0])):
-        question_max = np.max(x[col_id]) 
-        food_question_maxs.append(question_max)
-        x[col_id] /= question_max
+        col_data = x[col_id]
+
+        # replace weird low value with 0
+        col_data[col_data < MIN_THRESHOLD] = 0
+ 
+        # make between 0-1 with max
+        question_max = np.max(col_data)
+        maxs.append(question_max)
+        x[col_id] = col_data/question_max
+
+    print(f"question maxs: {maxs}")
     return x
 
 def get_ant_acids_bool_arr():
@@ -52,9 +67,10 @@ def get_ant_acids_bool_arr():
             if(drug.lower() in antacids):
                 contains = True
         y_arr.append(contains)
-    y = np.asarray(y_arr)
-    return y
 
+    y = np.asarray(y_arr)
+    print(f"percent taking drug: {np.sum(y)/len(y)}")
+    return y
 
 # get vals
 x = get_food_question_arr()
@@ -63,9 +79,6 @@ y = get_ant_acids_bool_arr()
 print("shapes")
 print(x.shape)
 print(y.shape)
-
-# format x - make between 0 and 1
-
 
 # format drugs to categories
 y_matrix = to_categorical(y)
@@ -78,19 +91,20 @@ x_train, x_test, y_train, y_test = train_test_split(x, y_matrix, random_state=7,
 
 # create the model
 model = Sequential()
-print(len(food_questions))
-model.add(Dense(32, input_shape=(len(food_questions), ), activation='relu'))
-for i in range(5):
-    model.add(Dropout(0.5)) # prevent overfitting
-    model.add(Dense(20, activation='relu'))
+model.add(Dense(INIT_DENSITY, input_shape=(len(food_questions), ), activation='relu'))
+for i in range(20):
+    model.add(Dropout(0.05)) # prevent overfitting
+    model.add(Dense(15, activation='relu'))
 
 model.add(Dense(2, activation='softmax'))
 
+# custom metrics
+
 # compile
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', 'mse'])
 
 # train
-model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test), callbacks=[tensorboard])
+model.fit(x_train, y_train, epochs=20, validation_data=(x_test, y_test), callbacks=[tensorboard])
 
 # save
 model.save("doctor.model")
--- run.py
+++ run.py
@@ -4,23 +4,22 @@ from keras.models import load_model
 from keras.utils import to_categorical
 
 # parse arguments
-parser = argparse.ArgumentParser(description='"i am god" - carl')
-parser.add_argument("-f", "--food", help="int representing food type")
+parser = argparse.ArgumentParser(description='doctor says hi')
+parser.add_argument("-c", "--count", help="total number of questions")
 args = parser.parse_args()
 
 # select a combination food type (DR1CCMTX)
-x = 0
-if args.food:
-    x = int(args.food)
+count = 0
+if args.count:
+    count = int(args.count)
 else:
-    print("must use -f [food]")
+    print("must use -c [int]")
     exit(0)
 
 MODEL_NAME = "doctor.model"
 model = load_model(MODEL_NAME)
 
-
-for x in range(99):
-    prediction = model.predict(np.asarray([x]))
-    print(f"{x}: {prediction}")
+profile = []
+prediction = model.predict(np.asarray(profile))
+print(f"{x}: {prediction}")
 
