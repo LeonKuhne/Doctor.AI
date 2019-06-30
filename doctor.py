c4946ad6b34feab39331c1bf2bf6ae9feb275bfa
Leon Kuhne <lxk1170@rit.edu>
2019-06-29 22:22:05 -0400

    switch to binary

--- doctor.py
+++ doctor.py
@@ -22,15 +22,33 @@ data = pd.merge(medical_data, food_data, left_on="SEQN", right_on="SEQN")
 # DR1CCMTX - combination food type
 # RXDDRUG - perscription medications
 #food_data = data['DR1CCMTX']
-#drug_data = data['RXDDRUG']
+drug_data = data['RXDDRUG']
 
 # convert categories to id's
 food_ids = data.groupby('DR1CCMTX').ngroup()
-drug_ids = data.groupby('RXDDRUG').ngroup()
+#drug_ids = data.groupby('RXDDRUG').ngroup()
 
 # convert to numpy arrays
 x = food_ids.values
-y = drug_ids.values
+y_categories = drug_data.values
+#y = drug_ids.values
+
+# convert array to binary
+y_arr = []
+antacids = ['omeprazzole', 'pantoprazole', 'esomeprazole', 'lansoprazole', 'ranitidine', 'cimetidine', 'famotidine']
+for drugs_encoded in y_categories:
+    drugs = drugs_encoded.decode("utf-8")
+    contains = False
+    for drug in drugs.split("; "):
+        if(drug.lower() in antacids):
+            contains = True
+    y_arr.append(contains)
+
+y = np.asarray(y_arr)
+
+print("shapes")
+print(x.shape)
+print(y.shape)
 
 # format y
 y_matrix = to_categorical(y)
@@ -39,22 +57,20 @@ y_matrix = to_categorical(y)
 x_train, x_test, y_train, y_test = train_test_split(x, y_matrix, random_state=7, test_size=0.2)
 
 # determine num inputs and outputs
-num_foods = len(np.unique(x))
-num_drugs = len(np.unique(y))
+#num_foods = len(np.unique(x))
+#num_drugs = len(np.unique(y))
 
 # create the model
 model = Sequential()
 model.add(Dense(32, input_shape=(1,), activation='relu'))
 model.add(Dropout(0.5)) # prevent overfitting
-model.add(Dense(num_drugs, activation='softmax'))
+model.add(Dense(2, activation='softmax'))
 
 # compile
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # train
 model.fit(x_train, y_train, epochs=20, validation_data=(x_test, y_test), callbacks=[tensorboard])
 
 # save
 model.save("doctor.model")
-
-
---
+++ run.py
@@ -0,0 +1,26 @@
+import argparse
+import numpy as np
+from keras.models import load_model
+from keras.utils import to_categorical
+
+# parse arguments
+parser = argparse.ArgumentParser(description='"i am god" - carl')
+parser.add_argument("-f", "--food", help="int representing food type")
+args = parser.parse_args()
+
+# select a combination food type (DR1CCMTX)
+x = 0
+if args.food:
+    x = int(args.food)
+else:
+    print("must use -f [food]")
+    exit(0)
+
+MODEL_NAME = "doctor.model"
+model = load_model(MODEL_NAME)
+
+
+for x in range(90):
+    prediction = model.predict(np.asarray([x]))
+    print(prediction)
+
